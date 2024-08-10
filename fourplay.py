a83bce98f8ca924cfe626d2f1e8eb2678d970d2b
Vrushank <raovrushank@gmail.com>
2024-08-10 19:16:13 +0530

    Added routes and demodularized

--- fourplay.py
+++ fourplay.py
@@ -1,7 +1,25 @@
-from flask import render_template, url_for, flash, redirect
-from fourplay import app
-from fourplay.forms import RegistrationForm, LoginForm
-from fourplay.models import User, inventory
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+class inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(40), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
 
 @app.route("/")
 def home():
@@ -26,3 +44,6 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- run.py
+++
@@ -1,4 +0,0 @@
-from fourplay import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
