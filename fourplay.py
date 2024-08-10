0d2177ac6091464ca34ac5e879e09412a1b51916
Vrushank <raovrushank@gmail.com>
2024-08-11 00:03:17 +0530

    Added login and register links

--- fourplay.py
+++ fourplay.py
@@ -9,8 +9,8 @@ db = SQLAlchemy(app)
 
 app.app_context().push()
 
-class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+class Volunteer(db.Model):
+    volunteer_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
@@ -20,6 +20,12 @@ class inventory(db.Model):
     item = db.Column(db.String(40), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
+# phone_no - primary key because we want him to come only once and we keep it for OTP. If phone_no exists in db, redirect to home page
+# location is for determining red zone or not
+class Victim(db.Model):
+    phone_no = db.Column(db.BigInteger, primary_key=True, unique=True, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
 
 @app.route("/")
 def home():
@@ -45,5 +51,6 @@ def login():
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+# Remove while deploying
 if __name__ == '__main__':
     app.run(debug=True)
