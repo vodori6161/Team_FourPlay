4e6c5ff522c04048c0188413ecf123cb59361b1f
Vrushank <raovrushank@gmail.com>
2024-08-10 18:43:28 +0530

    Added .py files

---
+++ fourplay/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, url_for, flash, redirect
+from fourplay import app
+from fourplay.forms import RegistrationForm, LoginForm
+from fourplay.models import User, inventory
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+    
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
---
+++ run.py
@@ -0,0 +1,4 @@
+from fourplay import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
